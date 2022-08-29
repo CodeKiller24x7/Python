cfd828484940970498e2f35a1249ceacce932cee
Darshan kanani <darshankanani0007@gmail.com>
2022-08-29 08:29:24 +0530

    new

---
+++ Latter.py
@@ -0,0 +1,16 @@
+from xmlrpc.server import _DispatchArity4
+
+
+latter = '''Dear <|NAME|>
+You Are Selected!!
+
+Date : <|DATE|>
+'''
+
+name=input("Enter Your Name : ")
+date=input("Enter Date : ")
+
+latter=latter.replace("<|NAME|>",name)
+latter=latter.replace("<|DATE|>",date)
+
+print(latter)
